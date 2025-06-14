52270f148b966c08ed5ca4037986d314cd5d0369
Dennis Scherdt <dennisscherdt@gmail.com>
2025-06-14 19:32:16 -0400

    Update email address. Didn't realize I already owned the one I wanted lmao

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,7 +3,7 @@
 		<h1 class="text-4xl font-bold">Contact</h1>
 		<br />
 		<p class="text-lg">Dennis Scherdt</p>
-		<p class="text-lg">dennis@ishhsound.com</p>
+		<p class="text-lg">dennis@ishhmusic.com</p>
 	</div>
 </template>
 
