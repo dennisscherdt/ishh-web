2ce344911fceca6b844dadb54ebe7862ec206651
Dennis Scherdt <dennisscherdt@gmail.com>
2025-06-14 12:11:56 -0400

    Add logo and hamburger to header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,3 +1,11 @@
 <template>
-	<header class="h-24 bg-black"></header>
+	<header class="flex h-24 items-center justify-between bg-stone-900 p-4">
+		<HeaderLogo class="size-14 text-stone-200" />
+		<HeaderHamburger />
+	</header>
 </template>
+
+<script setup lang="ts">
+import HeaderLogo from '~/components/header/HeaderLogo.vue';
+import HeaderHamburger from '~/components/header/HeaderHamburger.vue';
+</script>
---
+++ components/header/HeaderHamburger.vue
@@ -0,0 +1,5 @@
+<template>
+	<button class="flex flex-col items-center gap-2">
+		<span v-for="i in 2" :key="i" class="h-1 w-8 rounded-full bg-stone-200"></span>
+	</button>
+</template>
