19e9428b614558e1ca51ff3b61bf543ffbb8362d
Dennis Scherdt <dennisscherdt@gmail.com>
2025-06-14 19:28:06 -0400

    Center align footer copyright text

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,7 +1,5 @@
 <template>
-	<footer class="h-24 bg-stone-950/60 p-4 backdrop-blur-sm">
-		<div class="flex items-center justify-between">
-			<p class="text-xs text-stone-400">© 2025 <i>ISHH</i></p>
-		</div>
+	<footer class="flex h-24 items-center justify-between bg-stone-950/60 p-4 backdrop-blur-sm">
+		<p class="text-xs text-stone-400">© 2025 <i>ISHH</i></p>
 	</footer>
 </template>
