80da277e2238da99cdf73ef3126f9b9b72e6a855
Dennis Scherdt <dennisscherdt@gmail.com>
2025-06-14 11:35:07 -0400

    Create initial app structure

--- app.vue
+++ app.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+	<AppHeader />
+	<main class="min-h-screen">
+		<NuxtPage />
+	</main>
+	<AppFooter />
 </template>
+
+<script setup lang="ts">
+import AppHeader from '~/components/AppHeader.vue';
+import AppFooter from '~/components/AppFooter.vue';
+</script>
---
+++ components/AppHeader.vue
@@ -0,0 +1,3 @@
+<template>
+	<header class="h-24 bg-black"></header>
+</template>
