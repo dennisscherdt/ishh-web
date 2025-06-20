1f484873ddc5de6acabe79f495dced44059c020a
Dennis Scherdt <dennisscherdt@gmail.com>
2025-06-20 17:25:22 -0400

    Add Old Soul Joints song links

--- pages/index.vue
+++ pages/index.vue
@@ -11,11 +11,12 @@
 				year: 2025,
 			}"
 			:links="{
-				apple: '#',
-				spotify: '#',
-				youtube: '#',
-				soundcloud: '#',
-				bandcamp: '#',
+				apple: 'https://music.apple.com/us/album/old-soul-joints/1819913860?i=1819913861',
+				spotify:
+					'https://open.spotify.com/track/49iYqpKai2iYPHrkYfrYd8?si=41de13f7ce1a40a5',
+				youtube: 'https://youtu.be/EgFy80yAwtc?si=LOHJiZZ5b2PXmQbi',
+				soundcloud: 'https://soundcloud.com/ishhhhhh/old-soul-joints',
+				bandcamp: 'https://ishhhh.bandcamp.com/track/old-soul-joints',
 			}"
 			class="mx-auto"
 		/>
