67bf0b3d6f186d8ec2445761828c51015728fe34
Dennis Scherdt <dennisscherdt@gmail.com>
2025-07-10 16:40:46 -0400

    Remove full name from contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,7 +2,6 @@
 	<div class="text-stone-100">
 		<h1 class="text-4xl font-bold">Contact</h1>
 		<br />
-		<p class="text-lg">Dennis Scherdt</p>
 		<p class="text-lg">dennis@ishhmusic.com</p>
 	</div>
 </template>
